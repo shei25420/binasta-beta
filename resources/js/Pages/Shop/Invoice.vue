9c5581289c21b778e5b635c39da6c45cb6ea095d
shei25420 <chegeeric66@gmail.com>
2023-01-20 21:19:10 +0300

    Dashboard realtime debug 2

--- resources/js/Pages/Shop/Invoice.vue
+++ resources/js/Pages/Shop/Invoice.vue
@@ -24,37 +24,37 @@ const paymentForm = useForm({
 const makePayment = () => {
     paymentForm.clearErrors();
     mpesaResponse.value = null;
-    if(!paymentForm.phone_number || paymentForm.phone_number.trim() == "") {
+    if (!paymentForm.phone_number || paymentForm.phone_number.trim() == "") {
         paymentForm.setError('phone_number', "Phone number is required");
     } else if (paymentForm.phone_number.length > 9) {
         paymentForm.setError('phone_number', 'Phone number is invalid. (Hint) Don\'t start with 0');
     }
 
-    if(!paymentForm.payment_type || paymentForm.payment_type.trim() == "") {
+    if (!paymentForm.payment_type || paymentForm.payment_type.trim() == "") {
         paymentForm.setError('payment_type', 'payment type is required');
     }
 
-    if(!paymentForm.order_ref || paymentForm.order_ref.trim() == "") {
+    if (!paymentForm.order_ref || paymentForm.order_ref.trim() == "") {
         paymentForm.setError('order_ref', 'Order ref is requireed');
     }
 
-    if(paymentForm.errors && Object.keys(paymentForm.errors).length) return;
+    if (paymentForm.errors && Object.keys(paymentForm.errors).length) return;
 
     const btn = document.getElementById("submitBtn");
     btn.innerText = "Making payment....";
     btn.setAttribute("disabled", true);
-    
+
     fetch(`/make_payment`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'X-XSRF-TOKEN': decodeURIComponent(document.cookie.split(";").filter(cookie => cookie.startsWith("XSRF-TOKEN="))[0].split("=")[1])
         },
-        body: JSON.stringify({phone_number: paymentForm.phone_number, payment_type: paymentForm.payment_type, order_ref: paymentForm.order_ref})
+        body: JSON.stringify({ phone_number: paymentForm.phone_number, payment_type: paymentForm.payment_type, order_ref: paymentForm.order_ref })
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 };
 
 onMounted(() => {
@@ -72,16 +72,16 @@ onMounted(() => {
                         "Content-Type": "application/json",
                         "X-XSRF-TOKEN": decodeURIComponent(document.cookie.split(";").filter(cookie => cookie.startsWith("XSRF-TOKEN"))[0].split("=")[1])
                     },
-                    body: JSON.stringify({payment_type: paymentForm.payment_type, order_ref: paymentForm.order_ref})
+                    body: JSON.stringify({ payment_type: paymentForm.payment_type, order_ref: paymentForm.order_ref })
                 })
-                .then(res => res.json())
-                .then(data => data.orderId)
-                .catch(err => console.log("make payment paypal err", err));
+                    .then(res => res.json())
+                    .then(data => data.orderId)
+                    .catch(err => console.log("make payment paypal err", err));
             },
             onApprove: (pData, actions) => {
                 console.log(pData);
                 captureForm.transform((data) => (
-                    {...data, ref: pData.orderID}
+                    { ...data, ref: pData.orderID }
                 )).post('/capture/paypal', {
                     onError: (err) => {
                         console.log("paypal capture err", err);
@@ -92,18 +92,18 @@ onMounted(() => {
     });
 
     window.Echo.private(`payments.${usePage().props.value.auth.user.id}`)
-    .listen('MpesaPaymentCaptured', e => {
-        mpesaResponse.value = { message: e.message };
-        if(parseInt(e.status) === 0) {
-            mpesaResponse.value.class = "primary";
-            window.location.href = "https://dashboard.binasta.co.ke/orders"; 
-        } else {
-            mpesaResponse.value.class = "danger";
-            const btn = document.getElementById("submitBtn");
-            btn.removeAttribute("disabled");
-            btn.innerText = "Make payment";
-        }
-    });
+        .listen('MpesaPaymentCaptured', e => {
+            mpesaResponse.value = { message: e.message };
+            if (parseInt(e.status) === 0) {
+                mpesaResponse.value.class = "primary";
+                window.location.href = "https://dashboard.binasta.co.ke/orders";
+            } else {
+                mpesaResponse.value.class = "danger";
+                const btn = document.getElementById("submitBtn");
+                btn.removeAttribute("disabled");
+                btn.innerText = "Make payment";
+            }
+        });
 });
 </script>
 
@@ -122,15 +122,16 @@ onMounted(() => {
                                     <div class="col-sm-6">
                                         <div class="invoice-name">
                                             <div class="logo">
-                                                <Link href="/"><img width="200" src="../../../assets/shop/imgs/theme/flogo.png"
-                                                        alt="logo"></Link>
+                                                <Link href="/"><img width="200"
+                                                    src="../../../assets/shop/imgs/theme/flogo.png" alt="logo"></Link>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="invoice-numb">
-                                            <h6 class="text-end mb-10 mt-20">{{ new
-                                                    Date(order.created_at).toDateString()
+                                            <h6 class="text-end mb-10 mt-20">{{
+                                                new
+                                                                                            Date(order.created_at).toDateString()
                                             }}</h6>
                                             <h6 class="text-end invoice-header-1">Invoice No: #{{ order.ref }}</h6>
                                         </div>
@@ -172,7 +173,8 @@ onMounted(() => {
                                                 </td>
                                                 <td class="text-center">ksh.{{ option.selling_price }}</td>
                                                 <td class="text-center">{{ option.pivot.quantity }}</td>
-                                                <td class="text-right">ksh.{{ option.pivot.quantity *
+                                                <td class="text-right">ksh.{{
+                                                    option.pivot.quantity *
                                                         option.selling_price
                                                 }}</td>
                                             </tr>
@@ -194,22 +196,24 @@ onMounted(() => {
                             </div>
                             <div class="invoice-bottom">
                                 <div class="row">
-                                    <div class="col-sm-6" v-if="!order.status">
+                                    <div class="col-sm-6" v-if="order.status == 0">
                                         <div class="row">
                                             <div class="col-sm-12">
                                                 <div>
                                                     <h3 class="invoice-title-1">Payment Method</h3>
                                                     <form action="">
                                                         <div class="form-check">
-                                                            <input class="form-check-input" v-model="paymentForm.payment_type" type="radio"
-                                                             :value="'mpesa'" id="flexRadioDefault1">
+                                                            <input class="form-check-input"
+                                                                v-model="paymentForm.payment_type" type="radio"
+                                                                :value="'mpesa'" id="flexRadioDefault1">
                                                             <label class="form-check-label" for="flexRadioDefault1">
                                                                 Mpesa
                                                             </label>
                                                         </div>
                                                         <div class="form-check">
                                                             <input class="form-check-input" type="radio"
-                                                            :value="'paypal'" v-model="paymentForm.payment_type" id="flexRadioDefault2">
+                                                                :value="'paypal'" v-model="paymentForm.payment_type"
+                                                                id="flexRadioDefault2">
                                                             <label class="form-check-label" for="flexRadioDefault2">
                                                                 Paypal / Credit Card
                                                             </label>
@@ -221,19 +225,27 @@ onMounted(() => {
                                             <div class="col-sm-12" v-show="paymentForm.payment_type == 'mpesa'">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <span style="padding: 11px" class="input-group-text" id="basic-addon1"><strong>+254</strong></span>
+                                                        <span style="padding: 11px" class="input-group-text"
+                                                            id="basic-addon1"><strong>+254</strong></span>
                                                     </div>
-                                                    <input type="text" v-model="paymentForm.phone_number" placeholder="Enter phone number" class="form-control">
+                                                    <input type="text" v-model="paymentForm.phone_number"
+                                                        placeholder="Enter phone number" class="form-control">
                                                 </div>
-                                                <div class="invalid-feedback" v-if="paymentForm.errors.phone_number">{{ paymentForm.errors.phone_number }}</div>
-                                                <div v-if="mpesaResponse" :class="`alert alert-${mpesaResponse.class}`" role="alert">
+                                                <div class="invalid-feedback" v-if="paymentForm.errors.phone_number">{{
+                                                    paymentForm.errors.phone_number
+                                                }}</div>
+                                                <div v-if="mpesaResponse" :class="`alert alert-${mpesaResponse.class}`"
+                                                    role="alert">
                                                     <strong>{{ mpesaResponse.message }}</strong>
                                                 </div>
                                                 <div class="form-group">
-                                                    <button @click.prevent="makePayment" id="submitBtn" class="btn btn-lg btn-custom btn-download hover-up">Make Payment</button>
+                                                    <button @click.prevent="makePayment" id="submitBtn"
+                                                        class="btn btn-lg btn-custom btn-download hover-up">Make
+                                                        Payment</button>
                                                 </div>
                                             </div>
-                                            <div id="paypalContainer" class="col-sm-12" v-show="paymentForm.payment_type == 'paypal'">
+                                            <div id="paypalContainer" class="col-sm-12"
+                                                v-show="paymentForm.payment_type == 'paypal'">
 
                                             </div>
                                         </div>
@@ -244,15 +256,20 @@ onMounted(() => {
                                             <p><strong>Binasta Limited</strong></p>
                                             <div class="mobile-social-icon mt-50 print-hide">
                                                 <h6>Follow Us</h6>
-                                                <a href="#"><img src="../../../assets/shop/imgs/theme/icons/icon-facebook-white.svg"
+                                                <a href="#"><img
+                                                        src="../../../assets/shop/imgs/theme/icons/icon-facebook-white.svg"
                                                         alt=""></a>
-                                                <a href="#"><img src="../../../assets/shop/imgs/theme/icons/icon-twitter-white.svg"
+                                                <a href="#"><img
+                                                        src="../../../assets/shop/imgs/theme/icons/icon-twitter-white.svg"
                                                         alt=""></a>
-                                                <a href="#"><img src="../../../assets/shop/imgs/theme/icons/icon-instagram-white.svg"
+                                                <a href="#"><img
+                                                        src="../../../assets/shop/imgs/theme/icons/icon-instagram-white.svg"
                                                         alt=""></a>
-                                                <a href="#"><img src="../../../assets/shop/imgs/theme/icons/icon-pinterest-white.svg"
+                                                <a href="#"><img
+                                                        src="../../../assets/shop/imgs/theme/icons/icon-pinterest-white.svg"
                                                         alt=""></a>
-                                                <a href="#"><img src="../../../assets/shop/imgs/theme/icons/icon-youtube-white.svg"
+                                                <a href="#"><img
+                                                        src="../../../assets/shop/imgs/theme/icons/icon-youtube-white.svg"
                                                         alt=""></a>
                                             </div>
                                         </div>
@@ -269,9 +286,11 @@ onMounted(() => {
 
 <style scoped>
 @import '../../../css/shop/main17e6.css';
+
 .invalid-feedback {
     display: block;
 }
+
 .invoice-1 .invoice-header {
     background: rgba(0, 0, 0, 0.04) url(../../../assets/shop/imgs/invoice/header-bg-1.png) top left repeat !important;
 }
