580839fa7b5488b4d8ae4fa6777010a91e80aa47
shei25420 <chegeeric66@gmail.com>
2023-01-20 21:06:38 +0300

    Dashboard realtime debug 2

--- routes/shop.php
+++ routes/shop.php
@@ -6,21 +6,19 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware("guest")->group(function () {
-    Route::get('/', [ShopController::class, 'index'])->name('shop.index');
-    Route::get('/products', [ShopController::class, 'fetchProducts']);
-    Route::get('/products/{slug}', [ShopController::class, 'show'])->name('shop.products.show');
+Route::get('/', [ShopController::class, 'index'])->name('shop.index');
+Route::get('/products', [ShopController::class, 'fetchProducts']);
+Route::get('/products/{slug}', [ShopController::class, 'show'])->name('shop.products.show');
 
-    Route::get('/checkout', [ShopController::class, 'checkout']);
+Route::get('/checkout', [ShopController::class, 'checkout']);
 
-    Route::get('/login', [AuthenticationController::class, 'create'])->name('shop.login.create');
-    Route::post('/login', [AuthenticationController::class, 'store'])->name('shop.login.store');
+Route::get('/login', [AuthenticationController::class, 'create'])->name('shop.login.create');
+Route::post('/login', [AuthenticationController::class, 'store'])->name('shop.login.store');
 
-    Route::get('/register', [RegisteredUserController::class, 'create']);
-    Route::post('/register', [ShopController::class, 'register']);
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [ShopController::class, 'register']);
 
-    Route::get('/terms', [MainController::class, "terms"]);
-});
+Route::get('/terms', [MainController::class, "terms"]);
 
 // Authenticated Routes
 Route::middleware("auth")->group(function () {
