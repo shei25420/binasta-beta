ad7acd65ef078228ebe54bc56e20484ef87271b4
shei25420 <chegeeric66@gmail.com>
2023-01-21 01:12:44 +0300

    Distributor List fix --

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -30,11 +30,6 @@ class DistributorController extends Controller
      */
     public function index()
     {
-        dd(Distributor::select('id', 'parent_distributor_id', 'ref', 'country_id', 'first_name', 'last_name', 'email', 'binapoints', 'gender', 'domain', 'verified', 'suspended', 'balance', 'created_at', DB::raw("COUNT(*) as total, YEAR(created_at) year, MONTH(created_at) month, DAY(created_at) day"))->with(['country' => function ($query) {
-            $query->select('id', 'name');
-        }, 'patron' => function ($query) {
-            $query->select('id', 'first_name', 'last_name');
-        }])->withCount('orders')->groupBy("day")->latest()->get());
         return Inertia::render('Admin/Distributors', [
             'distributors' => Distributor::select('id', 'parent_distributor_id', 'ref', 'country_id', 'first_name', 'last_name', 'email', 'binapoints', 'gender', 'domain', 'verified', 'suspended', 'balance', 'created_at', DB::raw("COUNT(*) as total, YEAR(created_at) year, MONTH(created_at) month, DAY(created_at) day"))->with(['country' => function ($query) {
                 $query->select('id', 'name');
