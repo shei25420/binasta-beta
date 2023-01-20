52c244f6de02bc9bb1eb83d19f18d15f2b953008
shei25420 <chegeeric66@gmail.com>
2023-01-21 02:48:25 +0300

    Multitenant create db dynamically debug 2 --

--- database/migrations/2022_10_03_100716_create_user_addresses_table.php
+++ database/migrations/2022_10_03_100716_create_user_addresses_table.php
@@ -13,20 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('user_addresses', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('user_address_id')->nullable();
-            
-            $table->string('ref')->unique();
-            $table->string('location')->nullable();
-            $table->string('phone_number')->nullable();
-            $table->unsignedTinyInteger('status')->default(0); // 0 -> pending, 1 -> paid
-            $table->unsignedBigInteger('amount');
+            $table->string('location');
+            $table->string('phone_number');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('user_address_id')->references('id')->on('user_addresses')->onDelete('cascade');
         });
     }
 
@@ -37,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('user_addresses');
     }
 };
