7c7c1946b7f6a7fe9334a8efa4459e6dc7c9de61
shei25420 <chegeeric66@gmail.com>
2023-01-21 02:45:22 +0300

    Multitenant create db dynamically debug 2

--- database/migrations/tenant/2022_12_31_010505_create_orders_table.php
+++ database/migrations/tenant/2022_12_31_010505_create_orders_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('user_address_id')->nullable();
             
             $table->string('ref')->unique();
             $table->string('location')->nullable();
@@ -26,7 +25,6 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('user_address_id')->references('id')->on('user_addresses')->onDelete('cascade');
         });
     }
 
