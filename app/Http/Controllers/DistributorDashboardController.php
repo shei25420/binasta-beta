c912df9b7f18722cf00971bb57c065404e3941ba
shei25420 <chegeeric66@gmail.com>
2023-01-21 11:54:22 +0300

    Distributor Chennel fix 1

--- app/Http/Controllers/DistributorDashboardController.php
+++ app/Http/Controllers/DistributorDashboardController.php
@@ -311,7 +311,7 @@ class DistributorDashboardController extends Controller
             event(new MpesaPaymentCaptured($transaction->distributorOrder->distributor->id, $status, $transaction, true));
             // event(new MpesaPaymentCaptured($transaction->distributorOrder->distributor->id, $status, $message, true));
         } catch (Exception $ex) {
-            event(new MpesaPaymentCaptured(auth("distributor")->id(), 1, $ex->getMessage(), true));
+            event(new MpesaPaymentCaptured(auth("distributor")->id(), 1, $transaction, true));
         }
     }
 
